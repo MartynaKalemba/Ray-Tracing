7ccdca4d1bdd28175ec453e9dc02a8cd8c58dee3
MartynaKalemba <Martynaakalemba@gmail.com>
2025-03-31 23:45:37 +0200

    Creating LightIntensity for pixels

---
+++ Lab1_Kalemba/LightIntensity.cs
@@ -0,0 +1,50 @@
+namespace RayTracing;
+
+public class LightIntensity
+{
+    protected
+    double r, g, b;
+
+    public LightIntensity(double R = 0, double G = 0, double B = 0)
+    {
+        r = R;
+        g = G;
+        b = B;
+    }
+
+    public double R => r;
+    public double G => g;
+    public double B => b;
+
+    public static LightIntensity operator +(LightIntensity a, LightIntensity b)
+    {
+        return new LightIntensity(a.r + b.r, a.g + b.g, a.b + b.b);
+    }
+
+    public static LightIntensity operator -(LightIntensity a, LightIntensity b)
+    {
+        return new LightIntensity(a.r - b.r, a.g - b.g, a.b - b.b);
+    }
+
+    public static LightIntensity operator *(LightIntensity a, LightIntensity b)
+    {
+        return new LightIntensity(a.r * b.r, a.g * b.g, a.b * b.b);
+    }
+
+    public static LightIntensity operator /(LightIntensity a, LightIntensity b)
+    {
+        return new LightIntensity(a.r / b.r, a.g / b.g, a.b / b.b);
+    }
+
+    public static LightIntensity operator *(LightIntensity a, double scalar)
+    {
+        return new LightIntensity(a.r * scalar, a.g * scalar, a.b * scalar);
+    }
+
+    public static LightIntensity operator /(LightIntensity a, double scalar)
+    {
+        return new LightIntensity(a.r / scalar, a.g / scalar, a.b / scalar);
+    }
+
+
+}
